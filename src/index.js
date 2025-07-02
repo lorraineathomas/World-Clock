dada2fd959630799c20724b740e7d3536939fa32
Lorraine Thomas <116093083+lorraineathomas@users.noreply.github.com>
2025-07-02 15:22:59 -0400

    Location updates to index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
-function updateDenverTime() {
-  //Denver
-  let denver = document.querySelector("#denver");
-  let denverDate = denver.querySelector(".date");
-  let denverTime = denver.querySelector(".time");
-  denverCurrentTime = moment().tz("America/Denver");
+function updateTime() {
+  // List locations
+  // Stockholm
+  let stockholm = document.querySelector("#stockholm");
+  if (stockholm) {
+    let stockholmDate = stockholm.querySelector(".date");
+    let stockholmTime = stockholm.querySelector(".time");
+    stockholmCurrentTime = moment().tz("Europe/Stockholm");
 
-  denverDate.innerHTML = denverCurrentTime.format("ddd, MMMM Do, YYYY");
-  denverTime.innerHTML = denverCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    stockholmDate.innerHTML = stockholmCurrentTime.format("ddd, MMMM Do, YYYY");
+    stockholmTime.innerHTML = stockholmCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  // Toronto
+  if (toronto) {
+    let toronto = document.querySelector("#toronto");
+    let torontoDate = toronto.querySelector(".date");
+    let torontoTime = toronto.querySelector(".time");
+    torontoCurrentTime = moment().tz("America/Toronto");
+
+    torontoDate.innerHTML = torontoCurrentTime
+      .tz("America/Toronto")
+      .format("ddd, MMMM Do, YYYY");
+    torontoTime.innerHTML = torontoCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
-setInterval(updateDenverTime, 1000);
+updateTime();
+setInterval(updateTime, 1000);
 
-function updateTorontoTime() {
-  //Toronto
-  let toronto = document.querySelector("#toronto");
-  let torontoDate = toronto.querySelector(".date");
-  let torontoTime = toronto.querySelector(".time");
-  torontoCurrentTime = moment().tz("America/Toronto");
-
-  torontoDate.innerHTML = torontoCurrentTime
-    .tz("America/Toronto")
-    .format("ddd, MMMM Do, YYYY");
-  torontoTime.innerHTML = torontoCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+function updateCity(event) {
+  let timezone = event.target.value;
+  let name = timezone.replaceAll("_", " ").split("/")[1];
+  let currentCityTime = moment().tz(timezone);
+  let cityChanged = document.querySelector(".locations");
+  cityChanged.innerHTML = `
+    <div class="city" id="stockholm">
+          <div id="city-time">
+            <h2>${name}</h2>
+            <div class="date">${currentCityTime.format(
+              "dddd, MMMM Do, YYYY"
+            )}</div>
+          </div>
+          <div>
+            <div class="time">${currentCityTime.format(
+              "h:mm:ss"
+            )}<small>${currentCityTime.format("A")}</small></div>
+          </div>
+        </div>`;
 }
-setInterval(updateTorontoTime, 1000);
+
+let citiesSelect = document.querySelector("#city-select");
+citiesSelect.addEventListener("change", updateCity);
